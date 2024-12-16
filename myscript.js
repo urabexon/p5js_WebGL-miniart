3468711bba0696c19f4d8acbe44e11819184eb4d
urabexon <hiroki708.a48uk@gmail.com>
2024-12-16 23:48:40 +0900

    myscript setup

--- myscript.js
+++ myscript.js
@@ -6,7 +6,29 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 01:30:40 by urabex            #+#    #+#             */
-/*   Updated: 2024/12/15 01:30:41 by urabex           ###   ########.fr       */
+/*   Updated: 2024/12/16 23:48:05 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+var program;
+var w;
+var h;
+
+var slider = 0;
+var gui;
+
+function setup() {
+    sliderRange(0, 4, 1);
+    gui = createGui('p5.gui');
+    gui.addGlobals('slider');
+    
+    pixelDensity(1);
+    w = document.documentElement.scrollWidth;
+    h = document.documentElement.scrollHeight;
+    createCanvas(w, h, WEBGL);
+    gl = this.canvas.getContext('webgl');
+    rectMode(CENTER);
+    noStroke();
+    fill(1);
+}
 
